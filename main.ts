d7564daf0eeb01bba76cbc063503ab9235c5c849
yordanbn <71376497+yordanbn@users.noreply.github.com>
2020-09-16 10:54:55 +0200

    de omhoog

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showIcon(IconNames.Fabulous)
+})
 basic.forever(function () {
 	
 })
